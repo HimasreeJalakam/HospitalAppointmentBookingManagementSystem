5564f55e04879bcc3f7ab416f21cad715ea85715
Himasree Jalakam <2440176@cognizant.com>
2025-09-10 16:14:43 +0530

    Adding controllers

---
+++ API/Controllers/AppointmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.DbContext;
+
+namespace API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AppointmentController : Controller
+{
+    private readonly AppDbContext _context;
+    public AppointmentController(AppDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    [Route("/api/appointments")]
+    public IActionResult GetAppointments()
+    {
+        var appointments = _context.Appointments.ToList();
+        return Ok(appointments);
+    }
+}
