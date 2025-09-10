26df01b40e9370d4039eca628d48e4ec7c6917bb
Prachi Fulchand Kamble <2440161@cognizant.com>
2025-09-10 17:08:12 +0530

    added notification controller

---
+++ API/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.DbContext;
+using Models.Models;
+
+
+
+namespace API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class NotificationController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public NotificationController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllNotifications() =>
+Ok(await _context.Notifications.ToListAsync());
+
+}
+
+
