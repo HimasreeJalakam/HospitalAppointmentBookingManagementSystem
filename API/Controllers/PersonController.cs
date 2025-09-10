86f799d4fff104b91427c053b804a117f24b0465
Sai Harshitha Chennareddy <2440104@cognizant.com>
2025-09-10 16:35:15 +0530

    added Person Controller

---
+++ API/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.DbContext;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public PersonController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var persons = _context.People.ToList();
+            return Ok(persons);
+        }
+    }
+}
