12152624c01e1798f12a6f66c558a04acf86cc99
Himasree Jalakam <2440176@cognizant.com>
2025-09-11 09:17:04 +0530

    Adding Appoinment Controller EndPoints.

--- API/Controllers/AppointmentController.cs
+++ API/Controllers/AppointmentController.cs
@@ -12,10 +12,94 @@ public class AppointmentController : Controller
         _context = context;
     }
     [HttpGet]
-    [Route("/api/appointments")]
+    [Route("/api/getAppointments")]
     public IActionResult GetAppointments()
     {
         var appointments = _context.Appointments.ToList();
         return Ok(appointments);
+    }
+
+    [HttpGet]
+    [Route("/api/getAppointmentsByPersonId/{PersonId}")]
+    public IActionResult GetAppointmentsByPersonId(int PersonId)
+    {
+        var appointments = _context.Appointments
+            .Where(a => a.DoctorId == PersonId || a.PatientId == PersonId)
+            .ToList();
+        return Ok(appointments);
+    }
+
+    [HttpPost]
+    [Route("/api/createAppointments")]
+    public IActionResult AddAppointment(
+        [FromQuery] string TimeSlotId,
+        [FromQuery] DateTime AppointmentDate,
+        [FromQuery] int DoctorId,
+        [FromQuery] int PatientId,
+        [FromQuery] string Status
+        )
+    {
+        if (TimeSlotId == null || AppointmentDate == null || DoctorId == 0 || PatientId == 0 || Status == null)
+        {
+            return BadRequest("Invalid input");
+        }
+        var newAppointment = new Models.Models.Appointment
+        {
+            TimeSlotId = TimeSlotId,
+            AppointmentDate = AppointmentDate,
+            DoctorId = DoctorId,
+            PatientId = PatientId,
+            Status = Status,
+            CreatedAt = DateTime.Now,
+            IsDeleted = "No"
+        };
+        _context.Appointments.Add(newAppointment);
+        _context.SaveChanges();
+        return Ok(newAppointment);
+    }
+
+    [HttpPatch]
+    [Route("/api/updateAppointment")]
+    public IActionResult UpdateAppointment(
+        [FromQuery] int AppointmentId,
+        [FromQuery] string TimeSlotId,
+        [FromQuery] DateTime AppointmentDate,
+        [FromQuery] int DoctorId,
+        [FromQuery] int PatientId,
+        [FromQuery] string Status)
+    {
+        var AppointmentToUpdateId = _context.Appointments.Find(AppointmentId);
+        if (AppointmentToUpdateId == null)
+        {
+            return NotFound("Enter AppointmentId To Be Update");
+        }
+        AppointmentToUpdateId.AppointmentId = AppointmentId;
+        AppointmentToUpdateId.TimeSlotId = TimeSlotId;
+        AppointmentToUpdateId.AppointmentDate = AppointmentDate;
+        AppointmentToUpdateId.DoctorId = DoctorId;
+        AppointmentToUpdateId.PatientId = PatientId;
+        AppointmentToUpdateId.Status = Status;
+        AppointmentToUpdateId.CreatedAt = DateTime.Now;
+        AppointmentToUpdateId.IsDeleted = "No";
+
+        _context.Update(AppointmentToUpdateId);
+        _context.SaveChanges();
+        return Ok(AppointmentToUpdateId);
+
+    }
+
+    [HttpDelete]
+    [Route("/api/deleteAppointment")]
+    public IActionResult DeleteAppointment(
+        [FromQuery] int AppointmentId)
+    {
+        var AppointmentToDeleteId =  _context.Appointments.Find(AppointmentId);
+        if(AppointmentToDeleteId == null)
+        {
+            return NotFound("Enter AppointmentId To Be Deleted");
+        }
+        _context.Remove(AppointmentToDeleteId);
+        _context.SaveChanges(); 
+        return Ok(AppointmentToDeleteId);
     }
 }
